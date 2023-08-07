339ad7d1d547fd3280b3025cf00e25a25938f6a2
Rummut <kzantip@gmail.com>
2023-08-07 22:35:13 +0300

    Update Profile.js

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,7 +3,7 @@ import { UserStatistic } from "./user_statistic/UserStatistic";
 import user from './user.json';
 import { Container } from "./Profile.styled";
 
-export const Profile = ({}) => {
+export const Profile = () => {
   return (
     <Container>
       <UserInfo  items={user}/>
